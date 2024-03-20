6d643d2ce2ba12e729d6ecbb2938ed94ef728b17
AmaraCela <amaracela@gmail.com>
2024-03-20 21:09:55 +0100

    password hashing

--- src/controller/addOperator.ts
+++ src/controller/addOperator.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from "express";
 import { UserRepository } from "../repos/UserRepository";
 import { PhoneOperator } from "../models/PhoneOperator";
+import bcrypt from "bcryptjs";
 
 const db = require("../sequelize/models");
 
 export async function addOperator(req: Request, res: Response) {
     const { name, surname, email, password } = req.body;
     const userRepository = new UserRepository(db);
-    const operator = new PhoneOperator(name, surname, email, password); 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const operator = new PhoneOperator(name, surname, email, hashedPassword); 
     const result = await userRepository.save(operator);
     res.status(200).json(result);
 }
--- src/controller/agentController/updateMeeting.ts
+++ src/controller/agentController/updateMeeting.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 export function updateMeeting (req: Request, res: Response) {
-    const { meetingId, nextDate = null, purchaseMade = false } = req.body;
+    const { meetingId, time, place, nextDate = null, purchaseMade = false } = req.body;
 
     try {
         
