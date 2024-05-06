cee689e9567350a389e171439690d6862e063a68
unknown <mmaksuti21@epoka.edu.al>
2024-05-06 18:29:11 +0200

    Logout functionality

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-import { createSession, loginValidator } from "../controllers/SessionController";
+import { createSession, deleteSession, loginValidator } from "../controllers/SessionController";
 import { handleInputValidationErrors } from "../controllers/utils/ErrorHandler";
 
 router.post("/", loginValidator, handleInputValidationErrors, createSession);
+router.post("/logout", loginValidator, handleInputValidationErrors, deleteSession);
 
 module.exports = router;
